fe04db00cdf0202488a7a2504cffbedc66fc1d43
Tim Randall <trtimrandall@gmail.com>
2021-07-24 10:59:01 +1000

    Initial Commit

---
+++ PaperScissorsRock/PaperScissorsRockApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaperScissorsRockApp.swift
+//  PaperScissorsRock
+//
+//  Created by Tim Randall on 24/7/21.
+//
+
+import SwiftUI
+
+@main
+struct PaperScissorsRockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
